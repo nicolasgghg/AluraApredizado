9497b9398904e21c3050bc14e9f65d44d49cb37b
nicolasgghg <nicolasgghg12@gmail.com>
2025-10-30 08:38:49 -0300

    feat: finish criando sua primeira api e conectando ao front

--- criando sua primeira API e conectando ao front/java-screenmatch-web/src/main/java/br/com/alura/screenmatch/model/Categoria.java
+++ criando sua primeira API e conectando ao front/java-screenmatch-web/src/main/java/br/com/alura/screenmatch/model/Categoria.java
@@ -5,7 +5,9 @@ public enum Categoria {
     ROMANCE("Romance", "Romance"),
     COMEDIA("Comedy", "Comédia"),
     DRAMA("Drama", "Drama"),
-    CRIME("Crime", "Crime");
+    CRIME("Crime", "Crime"),
+    ANIMATION("Animation", "Animação"),
+    HORROR("Horror", "Terror");
 
     private String categoriaOmdb;
     private String categoriaPortugues;
