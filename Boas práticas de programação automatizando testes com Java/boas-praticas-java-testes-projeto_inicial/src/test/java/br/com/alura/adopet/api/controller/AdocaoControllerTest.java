9a0db2ff7e99b344d4a9866b30d4106ae3ff149c
nicolasgghg <nicolasgghg12@gmail.com>
2025-11-02 20:56:04 -0300

    feat: tests on service, controller e validators

---
+++ Boas práticas de programação automatizando testes com Java/boas-praticas-java-testes-projeto_inicial/src/test/java/br/com/alura/adopet/api/controller/AdocaoControllerTest.java
@@ -0,0 +1,79 @@
+package br.com.alura.adopet.api.controller;
+
+import br.com.alura.adopet.api.dto.SolicitacaoAdocaoDto;
+import br.com.alura.adopet.api.service.AdocaoService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureJsonTesters
+class AdocaoControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private AdocaoService service;
+
+    @Autowired
+    private JacksonTester<SolicitacaoAdocaoDto> jsonDto;
+
+    @Test
+    void deveriaDevolverCodigo400ParaSolicitacaoDeAdocaoComErros() throws Exception {
+        //ARRANGE
+        String json = "{}";
+
+        //ACT
+
+        var response = mvc.perform(
+                MockMvcRequestBuilders.post("/adocoes")
+                        .content(json)
+                        .contentType(MediaType.APPLICATION_JSON)
+        ).andReturn().getResponse();
+
+
+        //ASSERT
+        Assertions.assertEquals(400, response.getStatus());
+    }
+
+    @Test
+    void deveriaDevolverCodigo200ParaSolicitacaoDeAdocaoSemErros() throws Exception {
+        //ARRANGE
+//        String json = """
+//                {
+//                    "idPet": 1,
+//                    "idTutor": 1,
+//                    "motivo": "Motivo Qualquer"
+//                }
+//                """;
+
+        SolicitacaoAdocaoDto dto = new SolicitacaoAdocaoDto(1l, 1l, "Motivo qualquer");
+
+
+        //ACT
+
+        var response = mvc.perform(
+                MockMvcRequestBuilders.post("/adocoes")
+                        .content(jsonDto.write(dto).getJson())
+                        .contentType(MediaType.APPLICATION_JSON)
+        ).andReturn().getResponse();
+
+
+        //ASSERT
+        Assertions.assertEquals(200, response.getStatus());
+    }
+
+
+}
---
+++ Boas práticas de programação automatizando testes com Java/boas-praticas-java-testes-projeto_inicial/src/test/java/br/com/alura/adopet/api/service/AdocaoServiceTest.java
@@ -0,0 +1,110 @@
+package br.com.alura.adopet.api.service;
+
+import br.com.alura.adopet.api.dto.SolicitacaoAdocaoDto;
+import br.com.alura.adopet.api.model.Abrigo;
+import br.com.alura.adopet.api.model.Adocao;
+import br.com.alura.adopet.api.model.Pet;
+import br.com.alura.adopet.api.model.Tutor;
+import br.com.alura.adopet.api.repository.AdocaoRepository;
+import br.com.alura.adopet.api.repository.PetRepository;
+import br.com.alura.adopet.api.repository.TutorRepository;
+import br.com.alura.adopet.api.validacoes.ValidacaoSolicitacaoAdocao;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.*;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class AdocaoServiceTest {
+
+    @InjectMocks
+    private AdocaoService service;
+
+    @Mock
+    private AdocaoRepository repository;
+
+    @Mock
+    private PetRepository petRepository;
+
+    @Mock
+    private TutorRepository tutorRepository;
+
+    @Mock
+    private EmailService emailService;
+
+    @Spy
+    private List<ValidacaoSolicitacaoAdocao> validacoes = new ArrayList<>();
+
+    @Mock
+    private ValidacaoSolicitacaoAdocao validador1;
+
+    @Mock
+    private ValidacaoSolicitacaoAdocao validador2;
+
+    @Mock
+    private Pet pet;
+
+    @Mock
+    private Tutor tutor;
+
+    @Mock
+    private Abrigo abrigo;
+
+    private SolicitacaoAdocaoDto dto;
+
+    @Captor
+    private ArgumentCaptor<Adocao> adocaoCaptor;
+
+
+    @Test
+    void deveriaSalvarAdocaoAoSolicitar(){
+        //ARRANGE
+
+        SolicitacaoAdocaoDto dto = new SolicitacaoAdocaoDto(10l, 20l, "Motivo Qualquer");
+
+        BDDMockito.given(petRepository.getReferenceById(dto.idPet())).willReturn(pet);
+        BDDMockito.given(tutorRepository.getReferenceById(dto.idTutor())).willReturn(tutor);
+        BDDMockito.given(pet.getAbrigo()).willReturn(abrigo);
+
+        //ACT
+        service.solicitar(dto);
+
+        //ASSERT
+        BDDMockito.then(repository).should().save(adocaoCaptor.capture());
+        Adocao adocaoSalva = adocaoCaptor.getValue();
+        Assertions.assertEquals(pet, adocaoSalva.getPet());
+        Assertions.assertEquals(tutor, adocaoSalva.getTutor());
+        Assertions.assertEquals(dto.motivo(), adocaoSalva.getMotivo());
+
+    }
+
+    @Test
+    void deveriaChamarValidadoresDeAdocaoAoSolicitar(){
+        //ARRANGE
+
+        SolicitacaoAdocaoDto dto = new SolicitacaoAdocaoDto(10l, 20l, "Motivo Qualquer");
+
+        BDDMockito.given(petRepository.getReferenceById(dto.idPet())).willReturn(pet);
+        BDDMockito.given(tutorRepository.getReferenceById(dto.idTutor())).willReturn(tutor);
+        BDDMockito.given(pet.getAbrigo()).willReturn(abrigo);
+        validacoes.add(validador1);
+        validacoes.add(validador2);
+
+
+        //ACT
+        service.solicitar(dto);
+
+        //ASSERT
+        BDDMockito.then(validador1).should().validar(dto);
+        BDDMockito.then(validador2).should().validar(dto);
+
+    }
+
+
+}
