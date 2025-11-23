58f525561b12c2b340704dd4b403cdfcacbf130a
nicolasgghg <nicolasgghg12@gmail.com>
2025-11-23 15:44:20 -0300

    feat: add token generate

--- java-e-spring-security-proteja-suas-apis-rest/API/src/main/java/br/com/forum_hub/infra/seguranca/ConfiguracoesSeguranca.java
+++ java-e-spring-security-proteja-suas-apis-rest/API/src/main/java/br/com/forum_hub/infra/seguranca/ConfiguracoesSeguranca.java
@@ -3,6 +3,8 @@ package br.com.forum_hub.infra.seguranca;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -26,4 +28,10 @@ public class ConfiguracoesSeguranca {
     public PasswordEncoder encriptador() {
         return new BCryptPasswordEncoder();
     }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
 }
