f9ff6d723559d726c8f94a3b96118da15fad3209
nicolasgghg <nicolasgghg12@gmail.com>
2025-11-23 08:23:54 -0300

    feat: change security config

--- java-e-spring-security-proteja-suas-apis-rest/API/src/main/java/br/com/forum_hub/infra/seguranca/ConfiguracoesSeguranca.java
+++ java-e-spring-security-proteja-suas-apis-rest/API/src/main/java/br/com/forum_hub/infra/seguranca/ConfiguracoesSeguranca.java
@@ -3,16 +3,27 @@ package br.com.forum_hub.infra.seguranca;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class ConfiguracoesSeguranca {
 
     @Bean
-    public PasswordEncoder encriptador(){
+    public SecurityFilterChain filtrosSeguranca(HttpSecurity http) throws Exception {
+        return http
+                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .csrf(csrf -> csrf.disable())
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder encriptador() {
         return new BCryptPasswordEncoder();
     }
 }
