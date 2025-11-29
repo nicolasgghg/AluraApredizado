8fb2d59989f29f4f7b42edabd19ba2d17dfe77ae
nicolasgghg <nicolasgghg12@gmail.com>
2025-11-29 16:01:35 -0300

    feat: login page

---
+++ java-e-spring-security-proteja-suas-aplicacoes-web/3865-seguranca-java-main/src/main/java/med/voll/web_application/infra/security/ConfiguracoesSeguranca.java
@@ -0,0 +1,50 @@
+package med.voll.web_application.infra.security;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class ConfiguracoesSeguranca {
+
+    @Bean
+    public UserDetailsService dadosUsuariosCadastrados() {
+        UserDetails usuario1 = User.builder()
+                .username("joao@email.com")
+                .password("{noop}joao123")
+                .build();
+        UserDetails usuario2 = User.builder()
+                .username("maria@email.com")
+                .password("{noop}maria123")
+                .build();
+        UserDetails usuario3 = User.builder()
+                .username("iasmin@email.com")
+                .password("{noop}iasmin123")
+                .build();
+        return new InMemoryUserDetailsManager(usuario1, usuario2, usuario3);
+    }
+
+    @Bean
+    public SecurityFilterChain filtrosSeguranca(HttpSecurity http) throws Exception {
+
+        return http
+                .authorizeHttpRequests(req -> {
+                    req.requestMatchers("/css/**", "/js/**", "/assets/**").permitAll();
+                    req.anyRequest().authenticated();
+                })
+                .formLogin(form -> form.loginPage("/login")
+                        .defaultSuccessUrl("/")
+                        .permitAll())
+                .build();
+
+    }
+
+}
