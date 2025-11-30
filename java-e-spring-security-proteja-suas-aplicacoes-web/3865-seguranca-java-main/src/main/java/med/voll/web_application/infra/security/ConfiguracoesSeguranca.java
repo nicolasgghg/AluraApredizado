d355c58960aa8a223fd2f37d5daa208df6412c6b
nicolasgghg <nicolasgghg12@gmail.com>
2025-11-30 17:14:30 -0300

    feat: user login with security on password

--- java-e-spring-security-proteja-suas-aplicacoes-web/3865-seguranca-java-main/src/main/java/med/voll/web_application/infra/security/ConfiguracoesSeguranca.java
+++ java-e-spring-security-proteja-suas-aplicacoes-web/3865-seguranca-java-main/src/main/java/med/voll/web_application/infra/security/ConfiguracoesSeguranca.java
@@ -6,32 +6,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class ConfiguracoesSeguranca {
-
-    @Bean
-    public UserDetailsService dadosUsuariosCadastrados() {
-        UserDetails usuario1 = User.builder()
-                .username("joao@email.com")
-                .password("{noop}joao123")
-                .build();
-        UserDetails usuario2 = User.builder()
-                .username("maria@email.com")
-                .password("{noop}maria123")
-                .build();
-        UserDetails usuario3 = User.builder()
-                .username("iasmin@email.com")
-                .password("{noop}iasmin123")
-                .build();
-        return new InMemoryUserDetailsManager(usuario1, usuario2, usuario3);
-    }
 
     @Bean
     public SecurityFilterChain filtrosSeguranca(HttpSecurity http) throws Exception {
@@ -51,6 +32,11 @@ public class ConfiguracoesSeguranca {
                 .csrf(Customizer.withDefaults())
                 .build();
 
+    }
+
+    @Bean
+    public PasswordEncoder codificadorSenha(){
+        return new BCryptPasswordEncoder();
     }
 
 }
