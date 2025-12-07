124a3b2d011a248a877f780f5360d4be5ff0c805
nicolasgghg <nicolasgghg12@gmail.com>
2025-12-07 20:35:05 -0300

    feat: add Assync communication, orders and payment services

--- microsservicos-na-pratica-implementando-com-java-e-spring/pagamentos/src/main/java/br/com/alurafood/pagamentos/controller/PagamentoController.java
+++ microsservicos-na-pratica-implementando-com-java-e-spring/pagamentos/src/main/java/br/com/alurafood/pagamentos/controller/PagamentoController.java
@@ -52,7 +52,8 @@ public class PagamentoController {
         return ResponseEntity.noContent().build();
     }
 
-
-
-
+    @PatchMapping("/{id}/confirmar")
+    public void confirmarPagamento(@PathVariable @NotNull Long id){
+        service.confirmarPagamento(id);
+    }
 }
--- microsservicos-na-pratica-implementando-com-java-e-spring/pagamentos/src/main/java/br/com/alurafood/pagamentos/service/PagamentoService.java
+++ microsservicos-na-pratica-implementando-com-java-e-spring/pagamentos/src/main/java/br/com/alurafood/pagamentos/service/PagamentoService.java
@@ -2,6 +2,7 @@ package br.com.alurafood.pagamentos.service;
 
 
 import br.com.alurafood.pagamentos.dto.PagamentoDto;
+import br.com.alurafood.pagamentos.http.PedidoClient;
 import br.com.alurafood.pagamentos.model.Pagamento;
 import br.com.alurafood.pagamentos.model.Status;
 import br.com.alurafood.pagamentos.repository.PagamentoRepository;
@@ -12,6 +13,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class PagamentoService {
 
@@ -20,6 +23,9 @@ public class PagamentoService {
 
     @Autowired
     private ModelMapper modelMapper;
+
+    @Autowired
+    private PedidoClient pedido;
 
     public Page<PagamentoDto> obterTodos(Pageable paginacao) {
         return repository
@@ -51,6 +57,18 @@ public class PagamentoService {
 
     public void excluirPagamento(Long id) {
         repository.deleteById(id);
+    }
+
+    public void confirmarPagamento(Long id){
+        Optional<Pagamento> pagamento = repository.findById(id);
+
+        if (!pagamento.isPresent()) {
+            throw new EntityNotFoundException();
+        }
+
+        pagamento.get().setStatus(Status.CONFIRMADO);
+        repository.save(pagamento.get());
+        pedido.atualizaPagamento(pagamento.get().getPedidoId());
     }
 
 }
