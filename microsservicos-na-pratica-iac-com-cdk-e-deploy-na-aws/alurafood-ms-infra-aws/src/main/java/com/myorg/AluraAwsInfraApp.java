58ae1bca854e8544a3ff7e90cc5fae48156b2a18
nicolasgghg <nicolasgghg12@gmail.com>
2025-12-13 15:47:33 -0300

    feat: config Cluster, Vpc and FargateService

---
+++ microsservicos-na-pratica-iac-com-cdk-e-deploy-na-aws/alurafood-ms-infra-aws/src/main/java/com/myorg/AluraAwsInfraApp.java
@@ -0,0 +1,22 @@
+package com.myorg;
+
+import software.amazon.awscdk.App;
+import software.amazon.awscdk.Environment;
+import software.amazon.awscdk.StackProps;
+
+import java.util.Arrays;
+
+public class AluraAwsInfraApp {
+    public static void main(final String[] args) {
+        App app = new App();
+
+        AluraVpcStack vpcStack = new AluraVpcStack(app, "Vpc");
+        AluraClusterStack clusterStack = new AluraClusterStack(app, "Cluster", vpcStack.getVpc());
+        clusterStack.addDependency(vpcStack);
+
+        AluraServiceStack aluraServiceStack = new AluraServiceStack(app, "Service", clusterStack.getCluster());
+        aluraServiceStack.addDependency(clusterStack);
+        app.synth();
+    }
+}
+
