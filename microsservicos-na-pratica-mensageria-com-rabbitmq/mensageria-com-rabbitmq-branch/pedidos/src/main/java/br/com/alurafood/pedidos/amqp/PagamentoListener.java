2e4fc1a427b671c20dd24c6267a4a0374ead5f16
nicolasgghg <nicolasgghg12@gmail.com>
2025-12-19 10:05:02 -0300

    feat: convert messagem payment in send and recive mode

--- microsservicos-na-pratica-mensageria-com-rabbitmq/mensageria-com-rabbitmq-branch/pedidos/src/main/java/br/com/alurafood/pedidos/amqp/PagamentoListener.java
+++ microsservicos-na-pratica-mensageria-com-rabbitmq/mensageria-com-rabbitmq-branch/pedidos/src/main/java/br/com/alurafood/pedidos/amqp/PagamentoListener.java
@@ -1,5 +1,6 @@
 package br.com.alurafood.pedidos.amqp;
 
+import br.com.alurafood.pedidos.dto.PagamentoDto;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
@@ -8,8 +9,15 @@ import org.springframework.stereotype.Component;
 public class PagamentoListener {
 
     @RabbitListener(queues = "pagamento.concluido")
-    public void recebeMensagem(Message message){
-        System.out.println("Mensagem recebida: " + message.toString());
+    public void recebeMensagem(PagamentoDto pagamento){
+        String message = """
+                Dados do pagamento: %s
+                Número do pedido: %s
+                Valor R$: %s
+                Status: %s
+                """.formatted(pagamento.getId(), pagamento.getPedidoId(), pagamento.getValor(), pagamento.getStatus());
+
+        System.out.println("Mensagem recebida: " + message);
     }
 
 
