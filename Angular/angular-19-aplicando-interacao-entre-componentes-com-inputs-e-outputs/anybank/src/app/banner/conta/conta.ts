a1c47f3dba95d90244ff4b7067140d059d4e19f3
nicolasgghg <nicolasgghg12@gmail.com>
2025-12-26 16:18:52 -0300

    feat: create a banner, create and form

---
+++ Angular/angular-19-aplicando-interacao-entre-componentes-com-inputs-e-outputs/anybank/src/app/banner/conta/conta.ts
@@ -0,0 +1,12 @@
+import { CurrencyPipe } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-conta',
+  imports: [CurrencyPipe],
+  templateUrl: './conta.html',
+  styleUrl: './conta.css',
+})
+export class Conta {
+
+}
