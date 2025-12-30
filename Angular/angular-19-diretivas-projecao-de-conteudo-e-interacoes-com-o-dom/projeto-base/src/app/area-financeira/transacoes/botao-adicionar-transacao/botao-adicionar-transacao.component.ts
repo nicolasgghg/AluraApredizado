ed5cf7f9617618bb61f34ea6ab3ba2acc42b4416
nicolasgghg <nicolasgghg12@gmail.com>
2025-12-30 19:42:08 -0300

    feat: create a transação and update list

--- Angular/angular-19-diretivas-projecao-de-conteudo-e-interacoes-com-o-dom/projeto-base/src/app/area-financeira/transacoes/botao-adicionar-transacao/botao-adicionar-transacao.component.ts
+++ Angular/angular-19-diretivas-projecao-de-conteudo-e-interacoes-com-o-dom/projeto-base/src/app/area-financeira/transacoes/botao-adicionar-transacao/botao-adicionar-transacao.component.ts
@@ -1,24 +1,47 @@
-import { Component, effect, signal } from '@angular/core';
+import { Component, output, signal } from '@angular/core';
 import { BotaoComponent } from "../../../compartilhados/botao/botao.component";
 import { ModalComponent } from "../../../compartilhados/modal/modal.component";
+import { FormsModule } from '@angular/forms';
+import { KeyValuePipe } from '@angular/common';
+import { TipoTransacao, Transacao } from '../../compartilhados/transacao.model';
 
 @Component({
   selector: 'app-botao-adicionar-transacao',
-  imports: [BotaoComponent, ModalComponent],
+  imports: [BotaoComponent, ModalComponent, FormsModule, KeyValuePipe],
   templateUrl: './botao-adicionar-transacao.component.html',
   styleUrl: './botao-adicionar-transacao.component.css'
 })
 export class BotaoAdicionarTransacaoComponent {
-
   modalAberto = signal(false)
 
-  constructor() { 
-    effect(() => {
-      
-    })
+  tipoTransacao = TipoTransacao;
+
+  novaTransacaoForm = {
+    nome: '',
+    tipo: '',
+    valor: '',
+    data: '',
+    conta: ''
   }
+
+  transacaoCriada = output<Transacao>();
+
   abrirModal() {
     this.modalAberto.set(true);
+  }
+
+  aoSubmeter(){
+    const novaTransacao = new Transacao(
+      this.novaTransacaoForm.nome,
+      this.novaTransacaoForm.tipo as TipoTransacao,
+      Number(this.novaTransacaoForm.valor),
+      this.novaTransacaoForm.data,
+      this.novaTransacaoForm.conta
+    )
+
+    this.transacaoCriada.emit(novaTransacao);
+    this.modalAberto.set(false);
+    
   }
 
 }
