eb0fd4a402e7d40d3bec2dfaff316dea246ea44a
nicolasgghg <nicolasgghg12@gmail.com>
2025-12-30 20:05:05 -0300

    feat: modify my accounts, create a form, and update list

--- Angular/angular-19-diretivas-projecao-de-conteudo-e-interacoes-com-o-dom/projeto-base/src/app/area-financeira/area-financeira.component.ts
+++ Angular/angular-19-diretivas-projecao-de-conteudo-e-interacoes-com-o-dom/projeto-base/src/app/area-financeira/area-financeira.component.ts
@@ -14,69 +14,23 @@ import { Transacao, TipoTransacao } from './compartilhados/transacao.model';
 export class AreaFinanceiraComponent {
   saldo = 0;
 
-  transacoes = signal<Transacao[]>([
-    {
-      id: '5',
-      nome: '',
-      tipo: TipoTransacao.SAQUE,
-      valor: 200,
-      data: new Date('2025-02-20T00:00'),
-      conta: 'Switch Bank'
-    },
-    {
-      id: '4',
-      nome: 'Almoço',
-      tipo: TipoTransacao.SAQUE,
-      valor: 40,
-      data: new Date('2025-01-15T00:00'),
-      conta: 'Bytebank'
-    },
-    {
-      id: '3',
-      nome: '',
-      tipo: TipoTransacao.DEPOSITO,
-      valor: 400,
-      data: new Date('2025-01-10T00:00'),
-      conta: 'Bytebank'
-    },
-    {
-      id: '2',
-      nome: 'Freela (2ª parte)',
-      tipo: TipoTransacao.DEPOSITO,
-      valor: 200,
-      data: new Date('2024-10-01T00:00'),
-      conta: 'Anybank'
-    },
-    {
-      id: '1',
-      nome: 'Freela (1ª parte)',
-      tipo: TipoTransacao.DEPOSITO,
-      valor: 100,
-      data: new Date('2024-10-01T00:00'),
-      conta: 'Anybank'
-    },
-  ]);
-
-  contas: Conta[] = [
-    {
-      nome: 'Anybank',
-      saldo: 1000,
-    },
-    {
-      nome: 'Bytebank',
-      saldo: 0,
-    },
-    {
-      nome: 'Switch Bank',
-      saldo: 0,
-    },
-  ];
+  transacoes = signal<Transacao[]>([]);
+  contas = signal<Conta[]>([]);
 
   processarTransacao(transacao: Transacao){
     this.transacoes.update(transacoes => {
       return [
         transacao,
         ...transacoes
+      ]
+    })
+  }
+
+  processarConta(conta: Conta){
+    this.contas.update(contas => {
+      return [
+        conta,
+        ...contas
       ]
     })
   }
--- Angular/angular-19-diretivas-projecao-de-conteudo-e-interacoes-com-o-dom/projeto-base/src/app/area-financeira/contas/contas.component.ts
+++ Angular/angular-19-diretivas-projecao-de-conteudo-e-interacoes-com-o-dom/projeto-base/src/app/area-financeira/contas/contas.component.ts
@@ -1,16 +1,17 @@
-import { Component, input } from '@angular/core';
+import { Component, input, output } from '@angular/core';
 import { ContaComponent } from "./conta/conta.component";
 import { Conta } from '../compartilhados/conta.model';
 import { CardComponent } from "../compartilhados/card/card.component";
-import { BotaoAdicionarTransacaoComponent } from "../transacoes/botao-adicionar-transacao/botao-adicionar-transacao.component";
-import { BotaoComponent } from "../../compartilhados/botao/botao.component";
+import { BotaoAdicionarContaComponent } from "./botao-adicionar-conta/botao-adicionar-conta.component";
 
 @Component({
   selector: 'app-contas',
-  imports: [ContaComponent, CardComponent, BotaoComponent],
+  imports: [ContaComponent, CardComponent, BotaoAdicionarContaComponent],
   templateUrl: './contas.component.html',
   styleUrl: './contas.component.css'
 })
 export class ContasComponent {
   contas = input.required<Conta[]>();
+
+  contaCriada = output<Conta>();
 }
