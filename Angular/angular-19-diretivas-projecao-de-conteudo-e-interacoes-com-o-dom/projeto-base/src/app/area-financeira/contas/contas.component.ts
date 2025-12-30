aecbdc498f69fb9fa0c2a67bbd4971ba3a488355
nicolasgghg <nicolasgghg12@gmail.com>
2025-12-30 13:09:27 -0300

    feat: create a modal

--- Angular/angular-19-diretivas-projecao-de-conteudo-e-interacoes-com-o-dom/projeto-base/src/app/area-financeira/contas/contas.component.ts
+++ Angular/angular-19-diretivas-projecao-de-conteudo-e-interacoes-com-o-dom/projeto-base/src/app/area-financeira/contas/contas.component.ts
@@ -1,12 +1,13 @@
 import { Component, input } from '@angular/core';
-import { BotaoComponent } from "../../compartilhados/botao/botao.component";
 import { ContaComponent } from "./conta/conta.component";
 import { Conta } from '../compartilhados/conta.model';
 import { CardComponent } from "../compartilhados/card/card.component";
+import { BotaoAdicionarTransacaoComponent } from "../transacoes/botao-adicionar-transacao/botao-adicionar-transacao.component";
+import { BotaoComponent } from "../../compartilhados/botao/botao.component";
 
 @Component({
   selector: 'app-contas',
-  imports: [BotaoComponent, ContaComponent, CardComponent],
+  imports: [ContaComponent, CardComponent, BotaoComponent],
   templateUrl: './contas.component.html',
   styleUrl: './contas.component.css'
 })
